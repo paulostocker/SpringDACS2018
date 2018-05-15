a7bd46cc1f9ee009b961a6b5d05382d408475db9
PAULO STOCKER <paulo.stocker@univille.br>
2018-05-14 22:04:48 -0300

    Update 2018-05-14

--- src/main/java/br/univille/projdacs2018/controller/HomeController.java
+++ src/main/java/br/univille/projdacs2018/controller/HomeController.java
@@ -3,6 +3,7 @@ package br.univille.projdacs2018.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.spring5.view.ThymeleafView;
 
 @Controller
 public class HomeController {
@@ -10,6 +11,6 @@ public class HomeController {
 	@RequestMapping("/")
 	@ResponseBody
 	public String index() {
-		return "I don't believe";
+		return "{\"name\":\"Stocker\"}";
 	}
 }
